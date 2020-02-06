229f1ca1a41fbeea3bf630f817471657c16b495d
Alejandro García <45419684+mediadocena@users.noreply.github.com>
2020-02-06 18:21:40 +0100

    Ej 5_1 Donete

--- src/Ej_5_1/Ejemplo4.java
+++ src/Ej_5_1/Ejemplo4.java
@@ -3,34 +3,27 @@ import java.security.*;
 public class Ejemplo4 {
 	public static void main(String[] args) {
 		MessageDigest md;
+		Salieri ex = new Salieri();
 		try {
 			md = MessageDigest.getInstance("SHA-256");
-			String texto = "Esto es un texto plano.";
-			byte dataBytes[] = texto.getBytes();
-			md.update(dataBytes);// SE INTRODUCE TEXTO A RESUMIR
-			byte resumen[] = md.digest(); // SE CALCULA EL RESUMEN
-			System.out.println("Mensaje original: " + texto);
-			System.out.println("Número de bytes: " + md.getDigestLength());
-			System.out.println("Algoritmo: " + md.getAlgorithm());
-			
-			System.out.println("Mensaje resumen: " + new String(resumen));
-			System.out.println("Mensaje en Hexadecimal: "
-					+ Hexadecimal(resumen));
-			Provider proveedor=md.getProvider();
-			System.out.println("Proveedor: " + proveedor.toString());
+			String clave = ex.controlaStringSt("Introduza el clave");
+			String texto2 = ex.controlaStringSt("Introduza el texto2");
+			String texto3 = ex.controlaStringSt("Introduza el texto3");
+			byte claveBytes[] = clave.getBytes();
+			byte texto2Bytes[] = texto2.getBytes();
+			byte texto3Bytes[] = texto3.getBytes();
+			md.update(claveBytes);
+			byte resumenClave[] = md.digest();
+			md.update(texto2Bytes);
+			byte resumenTexto2[] = md.digest(claveBytes);
+			md.update(texto3Bytes);
+			byte resumenTexto3[] = md.digest(claveBytes);
+			System.out.println("Mensaje original Texto2: " + texto2);
+			System.out.println("Mensaje original Texto2: " + texto3);
+			System.out.println("Mensaje resumen Texto2: " + new String(resumenTexto2));
+			System.out.println("Mensaje resumen Texto2: " + new String(resumenTexto3));
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		}
-	}// main
-	// CONVIERTE UN ARRAY DE BYTES A HEXADECIMAL
-	static String Hexadecimal(byte[] resumen) {
-		String hex = "";
-		for (int i = 0; i < resumen.length; i++) {
-			String h = Integer.toHexString(resumen[i] & 0xFF);
-			if (h.length() == 1)
-				hex += "0";
-			hex += h;
-		}
-		return hex.toUpperCase();
-	}// Hexadecimal
+	}
 }// ..
